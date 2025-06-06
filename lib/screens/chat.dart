a53fb626f4749e2ed07dd7cb3970fc34162c2964
sanashajiya <ssanashajiya@gmail.com>
2025-06-06 13:02:49 +0530

    Implement push notifications using firebase_messaging and Cloud Functions

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,33 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+
+  fcm.subscribeToTopic('chat');
+
+    // final token = await fcm.getToken();
+    // print(token);
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,21 +36,17 @@ class ChatScreen extends StatelessWidget {
         title: Text("Flutter Chat"),
         actions: [
           IconButton(
-            onPressed: (){
+            onPressed: () {
               FirebaseAuth.instance.signOut();
-            }, 
-            icon: Icon(Icons.exit_to_app,
-            color: Theme.of(context).colorScheme.primary,
-            )
-          )
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
         ],
       ),
-      body: Column(
-        children: [
-          Expanded(child: ChatMessages()),
-          NewMessage(),
-        ],
-      ),
+      body: Column(children: [Expanded(child: ChatMessages()), NewMessage()]),
     );
   }
 }
